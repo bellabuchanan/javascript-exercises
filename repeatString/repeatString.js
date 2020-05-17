edd4e719af7022730fdf89583054cf77b171d7ee
Bella Buchanan <isabellabuchanan1@gmail.com>
2020-05-17 20:38:08 +0200

    finished problem

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, repetitions) {
+    
+    let string = ''
 
-}
+    if (repetitions < 0) {
+        return 'ERROR'
+    }
+
+    for (let i = 0; i<repetitions; i++) {
+        string += word
+    }
+    return string
+};
 
 module.exports = repeatString
