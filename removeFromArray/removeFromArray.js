0794478b1ab6db5664dd38e2d1ff0044ed8d13c9
Bella Buchanan <isabellabuchanan1@gmail.com>
2020-05-18 08:27:56 +0200

    completed remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArray = []
+    for (j=0; j<args.length; j++) {
+        for (i = 0; i<arr.length; i++) {
+            if (arr[i]===args[j]) {
+                arr.splice(i, 1)
+              }
+        }
+    }
+    
+return arr
 
-}
+};
 
 module.exports = removeFromArray
