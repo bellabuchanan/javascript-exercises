f6995b478a6f05df85c123d0df2568e5680113a5
Bella Buchanan <isabellabuchanan1@gmail.com>
2020-05-18 08:53:34 +0200

    finished sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    
+    if (start<0 || stop < 0) {
+        return 'ERROR'
+    }
 
+    if (typeof start !== "number" || typeof stop !== "number") {
+        return 'ERROR'
+    }
+
+    let sum = 0
+    let smaller = start
+    let larger = stop
+
+    
+    if (start > stop) {
+        smaller = stop
+        larger = start
+    }
+    for (let i = smaller; i<larger+1; i++) {
+        sum += i
+    }
+    return sum
 }
 
 module.exports = sumAll
